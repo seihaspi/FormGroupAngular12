14b9e47cbdaacc7012e08c3cd8ae21d3982f7c6e
seihaspi <younseyha988@gmail.com>
2021-07-13 02:56:38 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormControl } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'validation';
+  registerForm!: FormGroup;
+  ngOnInit(){
+    this.registerForm = new FormGroup(
+      {
+        firstname: new FormControl(),
+        lastname: new FormControl(),
+        email: new FormControl(),
+        password: new FormControl(),
+      }
+    );
+  }
+
+  onSubmit(form:FormGroup){
+    console.log(form.value.firstname);
+    console.log(form.value.lastname);
+    console.log(form.value.email);
+    console.log(form.value.password);
+  }
 }
